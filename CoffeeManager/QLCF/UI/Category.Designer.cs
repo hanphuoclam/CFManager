3b20a3369545b012a35a789068649017cf799cc3
Từ Vĩnh Lâm <vinhlam_168@hotmail.com.vn>
2017-12-09 16:25:36 +0700

    25/16/09/12 updata category

---
+++ CoffeeManager/QLCF/UI/Category.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace QLCF.UI
+{
+    partial class FormCategory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvListCategory = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thoátToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnAddCategory = new System.Windows.Forms.Button();
+            this.btnDeleteCategory = new System.Windows.Forms.Button();
+            this.btnEditCategory = new System.Windows.Forms.Button();
+            this.btnShowCategory = new System.Windows.Forms.Button();
+            this.txtNameCategory = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListCategory)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvListCategory
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvListCategory.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvListCategory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvListCategory.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvListCategory.Location = new System.Drawing.Point(3, 3);
+            this.dgvListCategory.Name = "dgvListCategory";
+            this.dgvListCategory.Size = new System.Drawing.Size(336, 290);
+            this.dgvListCategory.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(600, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.thoátToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // thoátToolStripMenuItem
+            // 
+            this.thoátToolStripMenuItem.Name = "thoátToolStripMenuItem";
+            this.thoátToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.thoátToolStripMenuItem.Text = "Thoát";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dgvListCategory);
+            this.panel1.Location = new System.Drawing.Point(12, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(342, 296);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.txtNameCategory);
+            this.panel2.Controls.Add(this.btnAddCategory);
+            this.panel2.Controls.Add(this.btnDeleteCategory);
+            this.panel2.Controls.Add(this.btnEditCategory);
+            this.panel2.Controls.Add(this.btnShowCategory);
+            this.panel2.Location = new System.Drawing.Point(360, 30);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(228, 290);
+            this.panel2.TabIndex = 3;
+            // 
+            // btnAddCategory
+            // 
+            this.btnAddCategory.BackColor = System.Drawing.Color.Black;
+            this.btnAddCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddCategory.ForeColor = System.Drawing.Color.White;
+            this.btnAddCategory.Location = new System.Drawing.Point(131, 196);
+            this.btnAddCategory.Name = "btnAddCategory";
+            this.btnAddCategory.Size = new System.Drawing.Size(94, 39);
+            this.btnAddCategory.TabIndex = 15;
+            this.btnAddCategory.Text = "Thêm";
+            this.btnAddCategory.UseVisualStyleBackColor = false;
+            this.btnAddCategory.Click += new System.EventHandler(this.btnAddCategory_Click);
+            // 
+            // btnDeleteCategory
+            // 
+            this.btnDeleteCategory.BackColor = System.Drawing.Color.Black;
+            this.btnDeleteCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeleteCategory.ForeColor = System.Drawing.Color.White;
+            this.btnDeleteCategory.Location = new System.Drawing.Point(13, 241);
+            this.btnDeleteCategory.Name = "btnDeleteCategory";
+            this.btnDeleteCategory.Size = new System.Drawing.Size(94, 39);
+            this.btnDeleteCategory.TabIndex = 14;
+            this.btnDeleteCategory.Text = "Xóa";
+            this.btnDeleteCategory.UseVisualStyleBackColor = false;
+            this.btnDeleteCategory.Click += new System.EventHandler(this.btnDeleteCategory_Click);
+            // 
+            // btnEditCategory
+            // 
+            this.btnEditCategory.BackColor = System.Drawing.Color.Black;
+            this.btnEditCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditCategory.ForeColor = System.Drawing.Color.White;
+            this.btnEditCategory.Location = new System.Drawing.Point(131, 241);
+            this.btnEditCategory.Name = "btnEditCategory";
+            this.btnEditCategory.Size = new System.Drawing.Size(94, 39);
+            this.btnEditCategory.TabIndex = 13;
+            this.btnEditCategory.Text = "Sửa";
+            this.btnEditCategory.UseVisualStyleBackColor = false;
+            this.btnEditCategory.Click += new System.EventHandler(this.btnEditCategory_Click);
+            // 
+            // btnShowCategory
+            // 
+            this.btnShowCategory.BackColor = System.Drawing.Color.Black;
+            this.btnShowCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShowCategory.ForeColor = System.Drawing.Color.White;
+            this.btnShowCategory.Location = new System.Drawing.Point(13, 196);
+            this.btnShowCategory.Name = "btnShowCategory";
+            this.btnShowCategory.Size = new System.Drawing.Size(94, 39);
+            this.btnShowCategory.TabIndex = 12;
+            this.btnShowCategory.Text = "Xem";
+            this.btnShowCategory.UseVisualStyleBackColor = false;
+            this.btnShowCategory.Click += new System.EventHandler(this.btnShowCategory_Click);
+            // 
+            // txtNameCategory
+            // 
+            this.txtNameCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.txtNameCategory.Location = new System.Drawing.Point(13, 49);
+            this.txtNameCategory.Name = "txtNameCategory";
+            this.txtNameCategory.Size = new System.Drawing.Size(212, 35);
+            this.txtNameCategory.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(8, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 29);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Tên danh mục:";
+            // 
+            // FormCategory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::QLCF.Properties.Resources._616x364;
+            this.ClientSize = new System.Drawing.Size(600, 325);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormCategory";
+            this.Text = "Quản lí danh mục";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListCategory)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvListCategory;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thoátToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNameCategory;
+        private System.Windows.Forms.Button btnAddCategory;
+        private System.Windows.Forms.Button btnDeleteCategory;
+        private System.Windows.Forms.Button btnEditCategory;
+        private System.Windows.Forms.Button btnShowCategory;
+    }
+}
---
+++ CoffeeManager/QLCF/UI/Category.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QLCF.Domain;
+using QLCF.Repository;
+using QLCF.Services;
+
+
+namespace QLCF.UI
+{
+    public partial class FormCategory : Form
+    {
+        private IProductCategoryService<ProductCategory> _serviceProdutCategory;
+        public FormCategory()
+        {
+            InitializeComponent();
+            IntData();
+            loadListCategory();
+            addBindingCategory();
+        }
+
+
+        #region Methods
+        void IntData()
+        {
+            _serviceProdutCategory = new ProductCategoryService(new ProductCategoryRepository());
+
+        }
+
+        void loadListCategory()
+        {
+            //IEnumerable<ProductCategory> list = _serviceProdutCategory.GetAll_S();
+            dgvListCategory.DataSource = _serviceProdutCategory.GetAll_S();
+            dgvListCategory.Columns["Products"].Visible = false;
+           
+            //DataTable data = new DataTable();
+            //foreach (ProductCategory item in list)
+            //{
+            //    DataRow row = data.NewRow();
+            //    row["ID"] = item.id;
+            //    row["Tên sản phẩm"] = item.name;
+            //}
+        }
+
+        void addBindingCategory()
+        {
+            txtNameCategory.DataBindings.Add(new Binding("Text", dgvListCategory.DataSource, "Name", true, DataSourceUpdateMode.Never));
+        }
+        void addCategory()
+        {
+            string nameCategory = txtNameCategory.Text;
+            if (_serviceProdutCategory.AddProductCategory_S(new ProductCategory(){ name = nameCategory }))
+            {
+                MessageBox.Show("Thêm thành công!!!");
+            }
+            loadListCategory();
+        }
+        void editCategory()
+        {
+            string nameCategory = txtNameCategory.Text;
+            int idProductCategory = Convert.ToInt32(dgvListCategory.SelectedCells[0].OwningRow.Cells["ID"].Value.ToString());
+            if (_serviceProdutCategory.EditProductCategory_S(new ProductCategory(){id = idProductCategory , name = nameCategory }))
+            {
+                MessageBox.Show("Sửa thành công!!!");
+            }
+            loadListCategory();
+
+        }
+        void deleteCategory()
+        {
+            int idProductCategory = Convert.ToInt32(dgvListCategory.SelectedCells[0].OwningRow.Cells["ID"].Value.ToString());
+            if (_serviceProdutCategory.DeleteProductCategory_S(idProductCategory))
+            {
+                MessageBox.Show("Xóa thành công!!!");
+            }
+            loadListCategory();
+
+        }
+        #endregion 
+        #region events
+        private void btnShowCategory_Click(object sender, EventArgs e)
+        {
+            loadListCategory();
+        }
+
+        private void btnAddCategory_Click(object sender, EventArgs e)
+        {
+            addCategory();
+        }
+
+        private void btnEditCategory_Click(object sender, EventArgs e)
+        {
+            editCategory();
+        }
+
+        private void btnDeleteCategory_Click(object sender, EventArgs e)
+        {
+            deleteCategory();
+        }
+        #endregion 
+    }
+}
